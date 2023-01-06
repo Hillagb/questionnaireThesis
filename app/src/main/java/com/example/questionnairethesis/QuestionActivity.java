8e6ee96da8bafa8d52b60a2338ab8f46d36c2e1e
hillagb <hillabla@gmail.com>
2023-01-06 16:04:50 +0200

    Finally!

--- app/src/main/java/com/example/questionnairethesis/QuestionActivity.java
+++ app/src/main/java/com/example/questionnairethesis/QuestionActivity.java
@@ -28,13 +28,16 @@ import com.google.firebase.storage.StorageReference;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 public class QuestionActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener{
     TextView tvTitle;
     Button[] buttons;
     MediaPlayer player;
     ImageButton ibAudio;
+    int filesCounter;
     String s;
+    ArrayList<String>Uris;
     private ArrayList<StorageReference> segments;
     private FirebaseFirestore db;
     @Override
@@ -45,15 +48,7 @@ public class QuestionActivity extends AppCompatActivity implements MediaPlayer.O
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReference();
         StorageReference listRef=storage.getReference();
-        listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
-            @Override
-            public void onSuccess(ListResult listResult) {
-                Log.d("Speeches", listRef.getName());
-                for (StorageReference prefix: listResult.getPrefixes()){
-                    Log.d("One Speech", prefix.getName());
-                }
-            }
-        });
+        Uris=new ArrayList<>();
         int num= (int) (Math.random()*324);
         tvTitle=findViewById(R.id.tvTitle);
 
@@ -65,12 +60,61 @@ public class QuestionActivity extends AppCompatActivity implements MediaPlayer.O
 
                 s="/gamliel"+num+".wav";
 
+                listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                            @Override
+                            public void onSuccess(ListResult listResult) {
+                                filesCounter=0;
+                                for (StorageReference item : listResult.getItems()) {
+                                    String temp="https://firebasestorage.googleapis.com/v0/b/questionnairethesis-8ee25.appspot.com/o/gamliel282.wav?alt=media&token=e9cb059b-4eea-4af6-b8f0-16de3afd59a8";
+                                    String tempName=item.toString().substring(item.toString().lastIndexOf("/")+1);
+                                    Log.d("Temp random name",tempName);
+                                    temp=temp.replace("gamliel282.wav",tempName);
+                                    Log.d("Temp random",temp);
+                                    if (temp!=null)
+                                        Uris.add(temp);
+                                    filesCounter++;
+                                }
+                                Random random=new Random();
+                                int randIndex=random.nextInt(filesCounter);
+                                Log.d("random number", String.valueOf(randIndex));
+                                int tempCounter=0;
+                                    if (tempCounter==randIndex){
+                                        try {
+                                            Log.d("random item ",Uris.get(randIndex));
+                                            player.setDataSource(Uris.get(randIndex));
+                                            //player.setDataSource("https://firebasestorage.googleapis.com/v0/b/questionnairethesis-8ee25.appspot.com/o/shashabiton8.wav?alt=media&token=e9cb059b-4eea-4af6-b8f0-16de3afd59a8");
+                                            player.prepare();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        try {
+                                            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                                                @Override
+                                                public void onPrepared(MediaPlayer mp) {
+                                                    player.start();
+                                                }
+                                            });
 
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                            }
+
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Uh-oh, an error occurred!
+                            }
+                        });
                 storageRef.child(s).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
                     public void onSuccess(Uri uri) {
                         try {
                             player.setDataSource(uri.toString());
+                            Log.d("not random ",uri.toString());
                             player.prepare();
                         } catch (IOException e) {
                             e.printStackTrace();
